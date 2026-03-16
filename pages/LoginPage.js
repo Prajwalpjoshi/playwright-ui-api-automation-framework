0e8313fb01d07daeaad9d47b4b97308b25f35f38
Prajwal Joshi <prajwaljoshi1856@gmail.com>
2026-03-16 10:10:13 +0530

    Fix Playwright CI configuration

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -2,16 +2,16 @@ class LoginPage {
 
  constructor(page){
   this.page = page
-  this.username = '#user-name'
-  this.password = '#password'
-  this.loginBtn = '#login-button'
+  this.username = page.locator('#user-name')
+  this.password = page.locator('#password')
+  this.loginBtn = page.locator('#login-button')
  }
 
  async login(user, pass){
 
-  await this.page.fill(this.username,user)
-  await this.page.fill(this.password,pass)
-  await this.page.click(this.loginBtn)
+  await this.username.fill(user)
+  await this.password.fill(pass)
+  await this.loginBtn.click()
 
  }
 
